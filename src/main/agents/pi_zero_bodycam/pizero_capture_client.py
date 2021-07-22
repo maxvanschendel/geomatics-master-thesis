f47808c0881c53f84b14c8d71f2d447016b66fae
maxvanschendel <maxvanschendel@gmail.com>
2021-07-22 12:37:54 +0200

    turned pi zero capture client script into a class

--- src/main/agents/pi_zero_bodycam/pizero_capture_client.py
+++ src/main/agents/pi_zero_bodycam/pizero_capture_client.py
@@ -3,41 +3,64 @@ import socket
 import struct
 import time
 import picamera
-
-# Connect a client socket to my_server:8000 (change my_server to the
-# hostname of your server)
-client_socket = socket.socket()
-client_socket.connect(('192.168.1.208', 8000))
+import argparse
 
 
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(0.1)
+class PiZeroCaptureClient:
+    def __init__(self, host: str, port: int, res: (int, int)):
+        self.host = host
+        self.port = port
+        self.res = res
+        self.client_socket = socket.socket()
 
-    # Note the start time and construct a stream to hold image data
-    # temporarily (we could write it directly to connection but in this
-    # case we want to find out the size of each capture first to keep
-    # our protocol simple)
-    stream = io.BytesIO()
-    for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-        # Write the length of the capture to the stream and flush to
-        # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
-        # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
-        
-        # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
-    # Write a length of zero to the stream to signal we're done
-    connection.write(struct.pack('<L', 0))
-finally:
-    connection.close()
-    client_socket.close()
+    def connect(self):
+        self.client_socket.connect((self.host, self.port))
+
+    def disconnect(self):
+        self.client_socket.close()
+
+    def stream_video(self):
+        # Make a file-like object out of the connection
+        connection = self.client_socket.makefile('wb')
+        try:
+            camera = picamera.PiCamera()
+            camera.resolution = self.res
+
+            # Let the camera warm up for 2 seconds
+            time.sleep(2)
+
+            # Note the start time and construct a stream to hold image data
+            # temporarily (we could write it directly to connection but in this
+            # case we want to find out the size of each capture first to keep
+            # our protocol simple)
+            stream = io.BytesIO()
+            for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+                # Write the length of the capture to the stream and flush to
+                # ensure it actually gets sent
+                connection.write(struct.pack('<L', stream.tell()))
+                connection.flush()
+                # Rewind the stream and send the image data over the wire
+                stream.seek(0)
+                connection.write(stream.read())
+
+                # Reset the stream for the next capture
+                stream.seek(0)
+                stream.truncate()
+            # Write a length of zero to the stream to signal we're done
+            connection.write(struct.pack('<L', 0))
+        finally:
+            connection.close()
+            self.disconnect()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Raspberry Pi Zero video streaming over websocket.')
+    parser.add_argument('host', type=str)
+    parser.add_argument('port', type=int)
+    parser.add_argument('xres', type=int)
+    parser.add_argument('yres', type=int)
+
+    args = parser.parse_args()
+    client = PiZeroCaptureClient(host=args['host'],
+                                 port=args['port'],
+                                 res=(args['xres'], args['yres']))
