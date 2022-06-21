046a64b65131212a1849b6f86bbc8dea161a86cd
Max van Schendel <maxvanschendel@gmail.com>
2022-06-21 15:25:15 +0200

    reworked map matching analysis to new input data format

--- src/evaluation/map_match_performance.py
+++ src/evaluation/map_match_performance.py
@@ -5,22 +5,34 @@ from utils.io import load_pickle
 
 from processing.parameters import MapMergeParameters
 
-m = 10
 
-
-def analyse_match_performance(target_matches, result_matches):
+def analyse_match_performance(partial_a, partial_b, ground_truth, matches):
+    
+    a_to_ground_truth = partial_a.match_nodes(ground_truth)
+    b_to_ground_truth = partial_b.match_nodes(ground_truth)
+    
+    a_matches = {a: v for a, v in a_to_ground_truth.keys()}
+    b_matches = {v: b for b, v in b_to_ground_truth.keys()}
+    
+    target_matches = [(a, b_matches[v]) for a, v in a_matches.items() if v in b_matches]
+    
+    
     true_positive = [
-        True for match in result_matches if match in target_matches]
+        True for match in matches if match in target_matches]
     false_positive = [
-        True for match in result_matches if match not in target_matches]
+        True for match in matches if match not in target_matches]
     false_negative = [
-        True for match in target_matches if match not in result_matches]
+        True for match in target_matches if match not in matches]
 
     # TODO: Validate that this is actually correct
-    accuracy = sum(true_positive) / len(result_matches)
+    accuracy = sum(true_positive) / len(matches)
     precision = sum(true_positive) / (sum(true_positive) + sum(false_positive))
     recall = sum(true_positive) / (sum(true_positive) + sum(false_negative))
-    f_1 = 2 * (precision*recall) / (precision+recall)
+    
+    if precision + recall:
+        f_1 = 2 * (precision*recall) / (precision+recall)
+    else:
+        f_1 = 0
 
     return {'accuracy': accuracy, 'precision': precision, 'recall': recall, 'f_1': f_1}
 
